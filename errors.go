d3c5c91230ebd7d653448ff10b084ac7caba13fb
dhaneshbs <dhanesh@scalekit.com>
2025-07-11 17:28:51 +0530

    remove unused error types

--- errors.go
+++ errors.go
@@ -4,13 +4,7 @@ import "errors"
 
 // Error types
 var (
-	ErrRefreshTokenRequired       = errors.New("refresh token is required")
-	ErrCodeAndRedirectUriRequired = errors.New("code and redirect uri is required")
-	ErrMissingRequiredHeaders     = errors.New("missing required headers")
-	ErrInvalidSecret              = errors.New("invalid secret")
-	ErrMessageTimestampTooOld     = errors.New("message timestamp too old")
-	ErrMessageTimestampTooNew     = errors.New("message timestamp too new")
-	ErrInvalidSignature           = errors.New("invalid signature")
-	ErrTokenExpired               = errors.New("token has expired")
-	ErrInvalidExpClaimFormat      = errors.New("invalid exp claim format")
+	ErrRefreshTokenRequired  = errors.New("refresh token is required")
+	ErrTokenExpired          = errors.New("token has expired")
+	ErrInvalidExpClaimFormat = errors.New("invalid exp claim format")
 )
