82283f7c859f05bb0d089333cf14f45d389d0dc0
dhaneshbs <dhanesh@scalekit.com>
2025-07-16 16:27:02 +0530

    upgrade release version

--- core.go
+++ core.go
@@ -15,7 +15,7 @@ import (
 const (
 	tokenEndpoint = "oauth/token"
 	jwksEndpoint  = "keys"
-	sdkVersion    = "Scalekit-Go/v1.0.5"
+	sdkVersion    = "Scalekit-Go/v2.0.1"
 )
 
 type coreClient struct {
